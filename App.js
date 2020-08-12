067a44da2aea58b8c5c24966f4c45812b7852091
JuanYut <juanyut97@hotmail.com>
2020-08-12 10:47:08 -0500

    API Call

--- App.js
+++ App.js
@@ -1,49 +1,36 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { StyleSheet, Text, View, SectionList } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 
 // * MAIN COMPONENT
 export default function App() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return (
+      <View sytle={styles.center}>
+        <Text>Cargando...</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <SectionList
-        sections={[
-          {
-            title: "Group 1",
-            data: [
-              { key: "1", name: "JuanYut" },
-              { key: "2", name: "Yolanda" },
-              { key: "3", name: "Trunks Sayayin" },
-              { key: "4", name: "Maria" },
-              { key: "5", name: "Genji" },
-            ],
-          },
-          {
-            title: "Group 2",
-            data: [
-              { key: "6", name: "JuanYut" },
-              { key: "7", name: "Yolanda" },
-              { key: "8", name: "Trunks Sayayin" },
-              { key: "9", name: "Maria" },
-              { key: "10", name: "Genji" },
-            ],
-          },
-          {
-            title: "Group 3",
-            data: [
-              { key: "11", name: "JuanYut" },
-              { key: "12", name: "Yolanda" },
-              { key: "13", name: "Trunks Sayayin" },
-              { key: "14", name: "Maria" },
-              { key: "15", name: "Genji" },
-            ],
-          },
-        ]}
+      <FlatList
+        data={users}
         renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
-        renderSectionHeader={({ section }) => (
-          <Text style={styles.section}>{section.title}</Text>
-        )}
-      />
+        keyboardDismissMode={(item) => String(item.id)}
+      ></FlatList>
     </View>
   );
 }
@@ -64,13 +51,9 @@ const styles = StyleSheet.create({
     borderBottomWidth: "#ccc",
     borderBottomWidth: 1,
   },
-  section: {
-    fontSize: 16,
-    fontWeight: "bold",
-    backgroundColor: "#eee",
-    paddingTop: 2,
-    paddingLeft: 10,
-    paddingRight: 10,
-    paddingBottom: 2,
+  center: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
