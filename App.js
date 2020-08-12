90aefddc4ba682f77a20f1f189c4cf91a9cea56e
JuanYut <juanyut97@hotmail.com>
2020-08-12 10:50:10 -0500

    ActivityIndicator

--- App.js
+++ App.js
@@ -1,49 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View, SectionList } from "react-native";
+import { StyleSheet, ActivityIndicator, View } from "react-native";
 
 // * MAIN COMPONENT
 export default function App() {
   return (
     <View style={styles.container}>
-      <SectionList
-        sections={[
-          {
-            title: "Group 1",
-            data: [
-              { key: "1", name: "JuanYut" },
-              { key: "2", name: "Yolanda" },
-              { key: "3", name: "Trunks Sayayin" },
-              { key: "4", name: "Maria" },
-              { key: "5", name: "Genji" },
-            ],
-          },
-          {
-            title: "Group 2",
-            data: [
-              { key: "6", name: "JuanYut" },
-              { key: "7", name: "Yolanda" },
-              { key: "8", name: "Trunks Sayayin" },
-              { key: "9", name: "Maria" },
-              { key: "10", name: "Genji" },
-            ],
-          },
-          {
-            title: "Group 3",
-            data: [
-              { key: "11", name: "JuanYut" },
-              { key: "12", name: "Yolanda" },
-              { key: "13", name: "Trunks Sayayin" },
-              { key: "14", name: "Maria" },
-              { key: "15", name: "Genji" },
-            ],
-          },
-        ]}
-        renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
-        renderSectionHeader={({ section }) => (
-          <Text style={styles.section}>{section.title}</Text>
-        )}
-      />
+      <ActivityIndicator size="large" color="#0000ff" />
     </View>
   );
 }
@@ -56,21 +19,5 @@ const styles = StyleSheet.create({
     alignItems: "stretch",
     justifyContent: "center",
     paddingTop: 22,
-  },
-  item: {
-    padding: 10,
-    fontSize: 22,
-    height: 50,
-    borderBottomWidth: "#ccc",
-    borderBottomWidth: 1,
-  },
-  section: {
-    fontSize: 16,
-    fontWeight: "bold",
-    backgroundColor: "#eee",
-    paddingTop: 2,
-    paddingLeft: 10,
-    paddingRight: 10,
-    paddingBottom: 2,
   },
 });
